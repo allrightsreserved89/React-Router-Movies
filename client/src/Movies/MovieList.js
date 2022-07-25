31de20db90860589fe45342ae533c9be11513411
allrightsreserved89 <100737435+allrightsreserved89@users.noreply.github.com>
2022-07-25 18:17:33 -0500

    finished

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import {Link} from 'react-router-dom';
+
+
+
 export default function MovieList(props) {
   return (
     <div className="movie-list">
@@ -15,6 +19,7 @@ function MovieDetails(props) {
 
   return (
     <div className="movie-card">
+      <Link to={`/movies/props`}
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
